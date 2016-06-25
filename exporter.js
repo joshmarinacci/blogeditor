14ecba77e094820c07ed25855b36833eaa4f474a
Josh Marinacci <joshua@marinacci.org>
2016-06-24 21:33:50 -0700

    refactor the josh to draft code

--- exporter.js
+++ exporter.js
@@ -8,6 +8,60 @@ var j2d_style_map = {
     'subheader':'header-two'
 };
 
+var d2j_style_map = {
+    'STRONG':'strong',
+    'BOLD':'strong',
+    'EMPHASIS':'emphasis'
+};
+
+var d2j_entity_map = {
+    'LINK':'link'
+};
+var d2j_block_map = {
+    'header-two':'subheader'
+};
+
+function styleChange(block,index) {
+    for(var i=0; i<block.entityRanges.length; i++) {
+        var r = block.entityRanges[i];
+        if(r.offset == index) {
+            return {
+                found:true,
+                range:r,
+                start:true
+            }
+        }
+        if(r.offset + r.length == index) {
+            return {
+                found:true,
+                range:r,
+                start:false
+            }
+        }
+    }
+    for(var i=0; i<block.inlineStyleRanges.length; i++) {
+        var r = block.inlineStyleRanges[i];
+        if(r.offset == index) {
+            return {
+                found:true,
+                range:r,
+                start:true
+            }
+        }
+        if(r.offset + r.length == index) {
+            return {
+                found:true,
+                range:r,
+                start:false
+            }
+        }
+    }
+
+    return {
+        found:false
+    }
+}
+
 var exporter = {
     JoshRawToDraftRaw: function(raw) {
         var entityMap = {};
@@ -99,125 +153,69 @@ var exporter = {
     },
 
 
-    DraftRawToJoshRaw: function(blob) {
-        function styleChange(block,index) {
-            for(var i=0; i<block.entityRanges.length; i++) {
-                var r = block.entityRanges[i];
-                if(r.offset == index) {
-                    return {
-                        found:true,
-                        range:r,
-                        start:true
-                    }
-                }
-                if(r.offset + r.length == index) {
-                    return {
-                        found:true,
-                        range:r,
-                        start:false
-                    }
-                }
-            }
-            for(var i=0; i<block.inlineStyleRanges.length; i++) {
-                var r = block.inlineStyleRanges[i];
-                if(r.offset == index) {
-                    return {
-                        found:true,
-                        range:r,
-                        start:true
-                    }
-                }
-                if(r.offset + r.length == index) {
-                    return {
-                        found:true,
-                        range:r,
-                        start:false
-                    }
-                }
-            }
+    d2j_block: function(block, entityMap) {
+        var blkout = {
+            type:'block',
+            style:'body',
+            content:[]
+        };
+        var chunk = {
+            type:'text',
+            text:""
+        };
 
-            return {
-                found:false
+        var stack = [];
+        for(var i=0; i<block.text.length; i++) {
+            var ch = block.text[i];
+            var r = styleChange(block,i);
+            if(r.found) {
+                if(r.start) {
+                    blkout.content.push(chunk);
+                    var span = {
+                        type:'span',
+                        style:'plain',
+                        meta:{},
+                        content:[]
+                    };
+                    if(typeof r.range.style !== 'undefined') {
+                        if(d2j_style_map[r.range.style]) span.style = d2j_style_map[r.range.style];
+                    }
+                    if(typeof r.range.key !== 'undefined') {
+                        var ent = entityMap[r.range.key];
+                        console.log("ent = ",ent);
+                        if(d2j_entity_map[ent.type]) {
+                            span.style = d2j_entity_map[ent.type];
+                            span.meta.href = ent.data.url;
+                        }
+                    }
+                    blkout.content.push(span);
+                    stack.push(span);
+                    chunk = {
+                        type:'text',
+                        text:ch
+                    }
+                } else {
+                    var span = stack.pop();
+                    span.content.push(chunk);
+                    chunk = {
+                        type:'text',
+                        text:ch
+                    }
+                }
+            } else {
+                chunk.text += ch;
             }
         }
 
+        blkout.content.push(chunk);
+        if(d2j_block_map[block.type]) blkout.style = d2j_block_map[block.type];
+        return blkout;
+    },
+    DraftRawToJoshRaw: function(blob) {
+
         console.log("doing an export");
         console.log("blob = ",blob);
-        var blocks = blob.blocks.map((block)=>{
-            var txt = block.text;
-            var chunks = [];
-            var last = 0;
-            var cstyle = {};
-            var chunk = {
-                type:'text',
-                text:"",
-            };
-
-            var stack = [];
-            for(var i=0; i<block.text.length; i++) {
-                var ch = block.text[i];
-                //console.log('ch = ', ch);
-
-                var r = styleChange(block,i);
-                if(r.found) {
-                    if(r.start) {
-                        console.log("doing start", r.range);
-                        chunks.push(chunk);
-                        var span = {
-                            type:'span',
-                            style:'plain',
-                            meta:{},
-                            content:[]
-                        };
-                        if(typeof r.range.style !== 'undefined') {
-                            console.log("doing a style change");
-                            if(r.range.style == 'STRONG') {
-                                span.style = 'strong';
-                            }
-                            if(r.range.style == 'EMPHASIS') {
-                                span.style = 'emphasis';
-                            }
-                        }
-                        if(typeof r.range.key !== 'undefined') {
-                            console.log("it's a real entity");
-                            var ent = blob.entityMap[r.range.key];
-                            console.log("ent = ",ent);
-                            if(ent.type == 'LINK') {
-                                span.style = 'link';
-                                span.meta.href = ent.data.url;
-                            }
-                        }
-                        chunks.push(span);
-                        stack.push(span);
-                        chunk = {
-                            type:'text',
-                            text:ch
-                        }
-                    } else {
-                        console.log("doing end", r.range);
-                        var span = stack.pop();
-                        span.content.push(chunk);
-                        chunk = {
-                            type:'text',
-                            text:ch
-                        }
-                    }
-                } else {
-                    chunk.text += ch;
-                }
-            }
-
-            chunks.push(chunk);
-            var blkout = {
-                type:'block',
-                style:'body',
-                content: chunks
-            };
-            if(block.type == 'header-two') {
-                blkout.style = 'subheader';
-            }
-            return blkout;
-        });
+        var blocks = blob.blocks.map((block) => exporter.d2j_block(block,blob.entityMap));
         console.log("out = " + JSON.stringify({content:blocks},null,'  '));
         var bkx = {
             type:'root',
