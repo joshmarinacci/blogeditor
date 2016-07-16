7bb5739fd2e7b9f251f8e947e20dd4d375639da4
Josh Marinacci <joshua@marinacci.org>
2016-07-16 10:08:12 -0700

    delete dead code

--- editor.jsx
+++ editor.jsx
@@ -498,7 +498,6 @@ class App extends React.Component {
     }
 
     render() {
-        const {editorState} = this.state;
         return (<div className="main vbox">
                 <div className="toolbar">
                     <button onClick={this.logState}>log state</button>
@@ -558,42 +557,4 @@ class App extends React.Component {
 }
 
 
-
-const styles = {
-    root: {
-        fontFamily: '\'Georgia\', serif',
-        padding: 20,
-        width: 600,
-    },
-    buttons: {
-        marginBottom: 10,
-    },
-    urlInputContainer: {
-        marginBottom: 10,
-    },
-    urlInput: {
-        fontFamily: '\'Georgia\', serif',
-        marginRight: 10,
-        padding: 3,
-    },
-    editor: {
-        border: '1px solid #ccc',
-        cursor: 'text',
-        minHeight: 80,
-        padding: 10,
-    },
-    button: {
-        marginTop: 10,
-        textAlign: 'center',
-    },
-    link: {
-        color: '#3b5998',
-        textDecoration: 'underline',
-    },
-};
-
-
-ReactDOM.render(
-    <App/>,
-    document.getElementById('target')
-);
+ReactDOM.render(<App/>,document.getElementById('target'));
